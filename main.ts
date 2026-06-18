97c3c53f5635281eea8ad13746ca844fde92d2c8
b9oo <rishsirc@proton.me>
2026-06-18 16:03:31 -0500

    Refactor portal creation functions for position setting
    Refactor createPortalA and createPortalB functions to set portal positions using x and y properties instead of left and top.

--- main.ts
+++ main.ts
@@ -3,45 +3,40 @@ namespace magicPortals {
     let portalA: Sprite = null
     let portalB: Sprite = null
 
-    /**
-     * Create Portal A
-     */
     //% block="create portal A at x %x y %y"
-export function createPortalA(x: number, y: number) {
-    portalA = sprites.create(img`
-        . . 5 5 5 . .
-        . 5 5 5 5 5 .
-        5 5 1 1 1 5 5
-        5 1 5 5 5 1 5
-        5 1 5 5 5 1 5
-        5 5 1 1 1 5 5
-        . 5 5 5 5 5 .
-        . . 5 5 5 . .
-    `, SpriteKind.Food)
+    export function createPortalA(x: number, y: number) {
+        portalA = sprites.create(img`
+            . . 5 5 5 . . .
+            . 5 5 5 5 5 . .
+            5 5 1 1 1 5 5 .
+            5 1 5 5 5 1 5 .
+            5 1 5 5 5 1 5 .
+            5 5 1 1 1 5 5 .
+            . 5 5 5 5 5 . .
+            . . 5 5 5 . . .
+        `, SpriteKind.Food)
 
-    portalA.left = x
-    portalA.top = y
-}
+        portalA.x = x
+        portalA.y = y
+    }
 
-export function createPortalB(x: number, y: number) {
-    portalB = sprites.create(img`
-        . . 9 9 9 . .
-        . 9 9 9 9 9 .
-        9 9 1 1 1 9 9
-        9 1 9 9 9 1 9
-        9 1 9 9 9 1 9
-        9 9 1 1 1 9 9
-        . 9 9 9 9 9 .
-        . . 9 9 9 . .
-    `, SpriteKind.Food)
+    //% block="create portal B at x %x y %y"
+    export function createPortalB(x: number, y: number) {
+        portalB = sprites.create(img`
+            . . 9 9 9 . . .
+            . 9 9 9 9 9 . .
+            9 9 1 1 1 9 9 .
+            9 1 9 9 9 1 9 .
+            9 1 9 9 9 1 9 .
+            9 9 1 1 1 9 9 .
+            . 9 9 9 9 9 . .
+            . . 9 9 9 . . .
+        `, SpriteKind.Food)
 
-    portalB.left = x
-    portalB.top = y
-}
+        portalB.x = x
+        portalB.y = y
+    }
 
-    /**
-     * Enable teleporting
-     */
     //% block="enable portal teleporting for %player"
     export function enableTeleporting(player: Sprite) {
         game.onUpdate(function () {
