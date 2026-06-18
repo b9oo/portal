2c9a8ab890d11190c06a7bff583869ea02d55ad3
b9oo <rishsirc@proton.me>
2026-06-18 15:47:07 -0500

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,57 @@
+namespace magicPortals {
+
+    let portalA: Sprite = null
+    let portalB: Sprite = null
+
+    /**
+     * Create Portal A
+     */
+    //% block="create portal A at x %x y %y"
+    export function createPortalA(x: number, y: number) {
+        portalA = sprites.create(img`
+            . . 5 5 5 . .
+            . 5 5 5 5 5 .
+            5 5 1 1 1 5 5
+            5 1 5 5 5 1 5
+            5 1 5 5 5 1 5
+            5 5 1 1 1 5 5
+            . 5 5 5 5 5 .
+            . . 5 5 5 . .
+        `, SpriteKind.Food)
+        portalA.setPosition(x, y)
+    }
+
+    /**
+     * Create Portal B
+     */
+    //% block="create portal B at x %x y %y"
+    export function createPortalB(x: number, y: number) {
+        portalB = sprites.create(img`
+            . . 9 9 9 . .
+            . 9 9 9 9 9 .
+            9 9 1 1 1 9 9
+            9 1 9 9 9 1 9
+            9 1 9 9 9 1 9
+            9 9 1 1 1 9 9
+            . 9 9 9 9 9 .
+            . . 9 9 9 . .
+        `, SpriteKind.Food)
+        portalB.setPosition(x, y)
+    }
+
+    /**
+     * Enable teleporting
+     */
+    //% block="enable portal teleporting for %player"
+    export function enableTeleporting(player: Sprite) {
+        game.onUpdate(function () {
+            if (portalA && portalB) {
+                if (player.overlapsWith(portalA)) {
+                    player.setPosition(portalB.x, portalB.y)
+                } else if (player.overlapsWith(portalB)) {
+                    player.setPosition(portalA.x, portalA.y)
+                }
+            }
+        })
+    }
+}
